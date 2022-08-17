d751c9c24ecd81076c80f356d7900fab6578fd29
Bektemir1 <kudajberdievbektemir@gmail.com>
2022-08-17 23:21:54 +0600

    modify: create event with labels and free users, rooms

--- src/modals/CreateEvent/CreateEvent.jsx
+++ src/modals/CreateEvent/CreateEvent.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+/* eslint-disable no-alert */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react'
 import classNames from 'classnames'
-import { DatePicker, Space, Select } from 'antd'
+import { DatePicker, Space, Divider, Select } from 'antd'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 import locale from 'antd/es/date-picker/locale/ru_RU'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import icons from '../../assets/icons'
 import style from './CreateEvent.module.scss'
 import Users from './Users'
+import eventDefault from './eventDefault'
+import api from '../../services/api'
+import { getEvents, getFreeRoomAndUsers } from '../../store/event/eventAction'
+import { setFreeRoom } from '../../store/event/eventSlice'
 
 const { Option } = Select
 
 function CreateEvent({ handleClose }) {
+  const dispatch = useDispatch()
   const selectedDate = useSelector((state) => state.date.selectedDate)
+  const labels = useSelector((state) => state.labels.labels)
   const hours = useSelector((state) => state.date.hours)
   const repeat = useSelector((state) => state.date.repeat)
+  const freeUsers = useSelector((state) => state.event.freeUsers)
+  const freeRooms = useSelector((state) => state.event.freeRooms)
+  const [searchUser, setSearchUser] = useState('')
 
+  const [event, setEvent] = useState({
+    ...eventDefault,
+    event_date: [moment(selectedDate).format('YYYY-MM-DD')],
+  })
+  const handleChange = (e) => {
+    setEvent({ ...event, [e.target.name]: e.target.value })
+  }
   const hourOption = hours.map((hour) => (
     <Option key={hour} value={hour}>
       {hour}
@@ -26,6 +46,54 @@ function CreateEvent({ handleClose }) {
       {item}
     </Option>
   ))
+
+  const marks = labels.map((mark) => (
+    <Option key={mark.id} value={`${mark.id}`}>
+      <span className={style.create_label} style={{ background: mark.color }} />
+      {mark.name}
+    </Option>
+  ))
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const invites = event.invitees.map((item) => item.id)
+    const n = parseInt(event.repetitions_count, 10)
+    await api.post('/event/', { ...event, invitees: invites, repetitions_count: n }).then(() => {
+      dispatch(getEvents())
+      dispatch(setFreeRoom([]))
+    })
+  }
+  const inviteUsers = (user) => {
+    if (event.invitees.includes(user)) {
+      alert('You already added')
+    } else {
+      setSearchUser('')
+      setEvent({ ...event, invitees: [...event.invitees, user] })
+    }
+  }
+  const getFreeUsers = (value) => {
+    dispatch(getFreeRoomAndUsers(event, value))
+  }
+  const renderOption = (menu) => (
+    <>
+      {menu}
+      <Divider
+        style={{
+          margin: '8px 0',
+        }}
+      />
+      <Space
+        style={{
+          padding: '0 8px 4px',
+        }}
+      >
+        <div className={classNames('d-flex justify-content-between', style.repeat_option)}>
+          <span>Повт-неделя</span>
+          <input onChange={handleChange} name="repetitions_count" />
+        </div>
+      </Space>
+    </>
+  )
+
   return (
     <div className={style.create_event}>
       <div style={{ maxWidth: '600px' }}>
@@ -34,10 +102,16 @@ function CreateEvent({ handleClose }) {
             <h2 id="exampleModalLabel">Новое событие</h2>
             <button type="button" className="btn-close" aria-label="close" onClick={handleClose} />
           </div>
-          <div className={style.create_body}>
+          <form onSubmit={onSubmit} className={style.create_body}>
             <label htmlFor="name">
               Название
-              <input type="text" id="name1" name="name" />
+              <input
+                value={event.name}
+                onChange={handleChange}
+                type="text"
+                id="name1"
+                name="name"
+              />
             </label>
             <span className={style.label_span}>Дата и время</span>
             <div
@@ -56,54 +130,92 @@ function CreateEvent({ handleClose }) {
                   showToday={false}
                   mode="date"
                   defaultValue={moment(selectedDate)}
+                  onSelect={(v) => {
+                    setEvent({ ...event, event_date: [moment(v).format('YYYY-MM-DD')] })
+                  }}
                 />
               </Space>
               <Space>
-                <Select showArrow={false} defaultValue="09:00" className="general_select hour">
+                <Select
+                  placeholder="12:00"
+                  onChange={(value) => setEvent({ ...event, start: value })}
+                  showArrow={false}
+                  className="general_select hour"
+                >
                   {hourOption}
                 </Select>
               </Space>
               -
               <Space>
-                <Select showArrow={false} defaultValue="18:00" className="general_select hour">
+                <Select
+                  onChange={(value) => setEvent({ ...event, end: value })}
+                  showArrow={false}
+                  className="general_select hour"
+                  placeholder="24:00"
+                >
                   {hourOption}
                 </Select>
               </Space>
               <Space>
-                <Select defaultValue="Не повторять" className="general_select select_repeat">
+                <Select
+                  className="general_select select_repeat"
+                  dropdownClassName="repeat_dropdown"
+                  style={{ width: 500 }}
+                  onChange={(value) => {
+                    setEvent({ ...event, repetition: value })
+                    getFreeUsers(value)
+                  }}
+                  dropdownRender={renderOption}
+                  placeholder="Повторить"
+                >
                   {repeatOption}
-                  <Option>
-                    <div>
-                      <span>Период повт. (неделя)</span>
-                      <input />
-                    </div>
-                  </Option>
                 </Select>
               </Space>
             </div>
             <label htmlFor="user">
               Добавить участников
-              <input type="text" id="user" name="user" />
-              <div className={style.add_user}>
-                <div>Bektemir Kudaiberdiev</div>
-                <div>Bektemir Kudaiberdiev</div>
-                <div>Bektemir Kudaiberdiev</div>
-              </div>
+              <input value={searchUser} onChange={(e) => setSearchUser(e.target.value)} />
+              {searchUser && freeUsers.length ? (
+                <div className={style.add_user}>
+                  {freeUsers
+                    .filter((i) => i.name.toLowerCase().includes(searchUser.toLowerCase()))
+                    .map((item) => (
+                      <div
+                        onClick={() => inviteUsers(item)}
+                        key={item.id}
+                        className="d-flex align-items-center mb-2"
+                      >
+                        <img alt="" src={icons.avatar} />
+                        <div>
+                          <h3>
+                            {`${item.name} `}
+                            {item.surname}
+                          </h3>
+                          <span>{item.positions[0]}</span>
+                        </div>
+                      </div>
+                    ))}
+                </div>
+              ) : null}
             </label>
             <div className="d-flex align-items-center justify-content-between">
-              <span className={style.label_span}>Участники (1)</span>
-              {/* <div className={style.invite_btn}>
-                <img src={icons.whitePlusSVG} alt="" />
-              </div> */}
+              <span className={style.label_span}>
+                Участники
+                {` (${event.invitees.length})`}
+              </span>
             </div>
-            <Users />
+            <Users invitees={event.invitees} />
             <span className={style.label_span}>Помещение</span>
             <div className={style.dates}>
               <Space className="select_full_width">
-                <Select defaultValue="Конферренц зал" className="general_select">
-                  <Option value="1">Конференц зал</Option>
-                  <Option value="2">Аудитория 1</Option>
-                  <Option value="3">Аудитория 2</Option>
+                <Select
+                  onChange={(value) => setEvent({ ...event, room: parseInt(value, 10) })}
+                  placeholder="Выберите помещение"
+                  className="general_select"
+                >
+                  {freeRooms?.map((room) => (
+                    <Option key={room.id}>{room.name}</Option>
+                  ))}
                 </Select>
               </Space>
             </div>
@@ -112,9 +224,11 @@ function CreateEvent({ handleClose }) {
               <div className="w-50">
                 <span className={style.label_span}>Рабочий</span>
                 <Space className="select_full_width margin_right">
-                  <Select defaultValue="Рабочий" className="general_select">
-                    <Option value="1">Рабочий</Option>
-                    <Option value="2">Личный</Option>
+                  <Select
+                    onChange={(value) => setEvent({ ...event, mark: value })}
+                    className="general_select"
+                  >
+                    {marks}
                   </Select>
                 </Space>
               </div>
@@ -122,20 +236,24 @@ function CreateEvent({ handleClose }) {
               <div className="w-50">
                 <span className={style.label_span}>Общедоступное</span>
                 <Space className="select_full_width">
-                  <Select defaultValue="Общедоступное" className="general_select">
-                    <Option value="1">Общедоступное</Option>
-                    <Option value="2">Личное</Option>
+                  <Select
+                    onChange={(value) => setEvent({ ...event, is_private: value })}
+                    defaultValue="Общедоступное"
+                    className="general_select"
+                  >
+                    <Option value={false}>Общедоступное</Option>
+                    <Option value={!false}>Личное</Option>
                   </Select>
                 </Space>
               </div>
             </div>
 
             <div className="d-flex justify-content-end">
-              <button onClick={handleClose} type="button" className={style.save}>
+              <button onClick={handleClose} type="submit" className={style.save}>
                 Сохранить
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
--- src/modals/CreateEvent/Users.jsx
+++ src/modals/CreateEvent/Users.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import icons from '../../assets/icons'
 import style from './CreateEvent.module.scss'
 
-function Users(props) {
-  console.log(props)
+function Users({ invitees }) {
   return (
     <div className={style.invite_box}>
-      <div className={style.invite_user}>
-        <img src={icons.avatar} alt="" />
-        <div>
-          <h4>Jenny Wilson</h4>
-          <span>Intern</span>
+      {invitees.map((user) => (
+        <div key={user.id} className={style.invite_user}>
+          <img src={icons.avatar} alt="" />
+          <div>
+            <h4>
+              {`${user.name} `}
+              {user.surname}
+            </h4>
+            <span>{user.positions[0]}</span>
+          </div>
         </div>
-      </div>
-      <div className={style.invite_user}>
-        <img src={icons.avatar} alt="" />
-        <div>
-          <h4>Jenny Wilson</h4>
-          <span>Intern</span>
-        </div>
-      </div>
+      ))}
     </div>
   )
+}
+
+Users.propTypes = {
+  invitees: PropTypes.array,
 }
 
 export default Users
